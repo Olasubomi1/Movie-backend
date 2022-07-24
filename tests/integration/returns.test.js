cea37fb6cb38a1051a1abd508aead4d7bbbc18bd
Olasubomi <shubbynlaa@gmail.com>
2022-07-24 19:06:14 +0100

    Refactored returns test code with Mosh's technique and also tested for no rentals found in returns route.

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -8,11 +8,21 @@ describe("/api/returns", () => {
   let customerId;
   let movieId;
   let rental;
+  let token;
+
+  const exec = () => {
+    return request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId, movieId });
+  };
+
   beforeEach(async () => {
     server = require("../../index");
 
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
 
     rental = new Rental({
       customer: {
@@ -34,25 +44,24 @@ describe("/api/returns", () => {
   });
 
   it("should return 401 if client is not logged in!", async () => {
-    const res = await request(server)
-      .post("/api/returns")
-      .send({ customerId, movieId });
+    token = "";
+    const res = await exec();
     expect(res.status).toBe(401);
   });
   it("should return 400 if customerId is not provided", async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post("/api/returns")
-      .set("x-auth-token", token)
-      .send({ movieId });
+    customerId = "";
+    const res = await exec();
+
     expect(res.status).toBe(400);
   });
   it("should return 400 if movieId is not provided", async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post("/api/returns")
-      .set("x-auth-token", token)
-      .send({ customerId });
+    movieId = "";
+    const res = await exec();
     expect(res.status).toBe(400);
+  });
+  it("should return 404 if no rental found for the customer/movie", async () => {
+    await Rental.remove({});
+    const res = await exec();
+    expect(res.status).toBe(404);
   });
 });
