57d2f17a1878ae8b5d397391f3ff0dc95ca9ba0f
Olasubomi <shubbynlaa@gmail.com>
2022-07-24 18:27:14 +0100

    Implemmented integration TDD for authentication of customerId and movieId for returns route.

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const { Rental } = require("../../models/rental");
+const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 
 describe("/api/returns", () => {
@@ -37,5 +38,21 @@ describe("/api/returns", () => {
       .post("/api/returns")
       .send({ customerId, movieId });
     expect(res.status).toBe(401);
+  });
+  it("should return 400 if customerId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ movieId });
+    expect(res.status).toBe(400);
+  });
+  it("should return 400 if movieId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ movieId });
+    expect(res.status).toBe(400);
   });
 });
